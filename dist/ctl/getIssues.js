b08078830750bcc3d54b92ae881a9ab595d99234
Raul <raulsmarius@yahoo.com>
2024-03-03 23:26:49 +0200

    Added get and post route

---
+++ dist/ctl/getIssues.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const getIssues = (req, res, next) => {
+    res.status(200).json([]);
+};
+exports.default = getIssues;
